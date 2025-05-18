c230af4793fcac2897159413675f37961079f270
sotogito <sotogitoarchive@gmail.com>
2025-05-18 23:29:53 +0900

    feat : 검색 조건 및 페이징 기반 주문 목록 조회 서비스 로직 구현

--- src/main/java/com/iybook/sales/dao/SalesMapper.java
+++ src/main/java/com/iybook/sales/dao/SalesMapper.java
@@ -1,5 +1,15 @@
 package com.iybook.sales.dao;
 
+import com.iybook.sales.dto.OrderDto;
+import com.iybook.sales.dto.SingleOrderPagingDto;
+import com.iybook.sales.dto.OrderRequestFilterDto;
+
+import java.util.List;
+
 public interface SalesMapper {
+
+    int selectOrderListCountByFilter(OrderRequestFilterDto filter);
+
+    List<OrderDto> selectOrderListByFilterWithPaging(SingleOrderPagingDto pagingInfo);
 
 }
--- src/main/java/com/iybook/sales/service/SalesServiceImpl.java
+++ src/main/java/com/iybook/sales/service/SalesServiceImpl.java
@@ -1,13 +1,52 @@
 package com.iybook.sales.service;
 
+import com.iybook.sales.constants.OrderTableInfo;
+import com.iybook.sales.dao.SalesMapper;
+import com.iybook.sales.dto.OrderDto;
+import com.iybook.sales.dto.SingleOrderPagingDto;
+import com.iybook.sales.dto.OrderListResponseDto;
+import com.iybook.sales.dto.OrderRequestFilterDto;
+import com.iybook.sales.util.PageUtil;
 import lombok.RequiredArgsConstructor;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Service
 public class SalesServiceImpl implements SalesService {
 
     private final SqlSessionTemplate sqlSession;
+    private final PageUtil pageUtil;
+
+    @Override
+    public OrderListResponseDto getOrderListAndPageInfoByFilter(int page, OrderRequestFilterDto filter) {
+        SalesMapper salesMapper = sqlSession.getMapper(SalesMapper.class);
+
+        int orderCountResult = salesMapper.selectOrderListCountByFilter(filter);
+        Map<String, Object> pageInfo = pageUtil.getPageInfo(
+                orderCountResult,
+                page,
+                OrderTableInfo.DISPLAY.getValue(),
+                OrderTableInfo.PAGE_PER_BLOCK.getValue()
+        );
+        SingleOrderPagingDto paging = new SingleOrderPagingDto(
+                (Integer) pageInfo.get("offset"),
+                (Integer) pageInfo.get("display"),
+                filter
+        );
+        List<OrderDto> orderListResult = salesMapper.selectOrderListByFilterWithPaging(paging);
+
+        return new OrderListResponseDto(
+                page,
+                (Integer) pageInfo.get("totalPage"),
+                (Integer) pageInfo.get("beginPage"),
+                (Integer) pageInfo.get("endPage"),
+                orderCountResult,
+                orderListResult
+        );
+    }
 
 }

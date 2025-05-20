84d1a46c2829db50bf7090cdbaee70b81c48d4fb
sotogito <sotogitoarchive@gmail.com>
2025-05-20 15:13:32 +0900

    chore : 주문 상태변경 결과 출력을 위한 dto 삭제
    - map으로 결과 넘김

--- src/main/java/com/iybook/sales/dto/OrderStatusChangeResult.java
+++
@@ -1,8 +0,0 @@
-package com.iybook.sales.dto;
-
-import java.util.List;
-
-public record OrderStatusChangeResult(List<String> successIds,
-                                      List<String> failedIds)
-{
-}

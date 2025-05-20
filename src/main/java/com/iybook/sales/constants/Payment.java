b3cd6d0318fff44488f02827fc9d44fedb112c74
sotogito <sotogitoarchive@gmail.com>
2025-05-20 15:11:18 +0900

    feat : payment Enum tax 퍼센트 값 상수 추가

--- src/main/java/com/iybook/sales/constants/Payment.java
+++ src/main/java/com/iybook/sales/constants/Payment.java
@@ -5,14 +5,25 @@ import lombok.Getter;
 @Getter
 public enum Payment {
 
-    CARD("카드"),
-    CASH("현금")
+    CARD("카드", 10),
+    CASH("현금", 8)
     ;
 
     private final String value;
+    private final int taxPercent;
 
-    Payment(String value) {
+    Payment(String value, int taxPercent) {
         this.value = value;
+        this.taxPercent = taxPercent;
+    }
+
+    public static int getTaxPercent(String paymentType) {
+        for (Payment payment : Payment.values()) {
+            if (payment.getValue().equalsIgnoreCase(paymentType)) {
+                return payment.getTaxPercent();
+            }
+        }
+        return 0;
     }
 
 }
